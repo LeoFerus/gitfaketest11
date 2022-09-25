fa72e7666a3297beeb3b4583cf2e7848650add91
Kim,Yehun <skhu201713005@gmail.com>
2022-09-25 19:34:08 +0900

    main メソッド修正

--- src/git_faketest1/Meindesu.java
+++ src/git_faketest1/Meindesu.java
@@ -2,7 +2,9 @@ package git_faketest1;
 
 public class Meindesu {
 	
-	public static void main(String[]args) {
-		new Ohayou().say();
-	}
+    public static void main(String[] args) {
+        Ohayou h = new Ohayou();
+        h.say();
+    }
+
 }
